ce1d8721ac34af1c407cf8e36703521926a57227
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2022-11-11 21:35:48 +0100

    Improvements to points and polygon classification.

--- R/classifications.R
+++ R/classifications.R
@@ -29,10 +29,10 @@
 
 
 # The same, reshuffled a bit to make more economic sense
-osm_point_polygon_class <- list(
+osm_point_polygon_class_uo <- list(
   food = list(amenity = .c(bar, cafe, fast_food, food_court, ice_cream, pub, restaurant)),
-  shopping = list(amenity = "marketplace", # TODO: distinguish bigger and smaller, formal vs. informal shopping??
-                  shop = "",
+  shopping = list(amenity = .c(marketplace, shop), # TODO: distinguish bigger and smaller, formal vs. informal shopping??
+                  shop = c("!no", "!vacant"), # All except generic statements (in those cases the primary feature is something else)
                   building = .c(retail, kiosk, supermarket, warehouse),
                   landuse = "retail"), # .c(department_store, mall, supermarket, wholesale)
   craft = list(craft = ""),
@@ -51,7 +51,7 @@ osm_point_polygon_class <- list(
                    highway = .c(crossing, mini_roundabout, motorway_junction, traffic_signals, turning_circle, turning_loop, bus_stop, services, speed_camera),
                    railway = "",
                    aerialway = "",
-                   waterway = "", # Be more specific? But probably rare tag in Africa anyway...
+                   waterway = c("!waterfall", "!stream", "!stream_end", "!rapids", "!tidal_channel", "!riverbank"), # Getting rid of natural features...
                    aeroway = "",
                    public_transport = "",
                    bridge = "",
@@ -65,34 +65,38 @@ osm_point_polygon_class <- list(
   communications = list(amenity = .c(internet_cafe, telephone),
                         telecom = "",
                         communication ="", # regrex/startsWith(), e.g. communication:mobile_phone =
+                        utility = "telecom",
                         man_made = .c(antenna, beacon, communication_tower, communications_tower, lighthouse, satellite_dish), # telescope
                         office = "telecommunication", # mast
                         "tower:type" = .c(communication, telecommunication, radar, radio, radio_transmitter)),
-  accommodation = list(tourism = .c(apartment, chalet, guest_house, hostel, hotel, motel, wilderness_hut, camp_pitch, camp_site),
+  accommodation = list(tourism = .c(apartment, chalet, guest_house, hostel, hotel, motel, wilderness_hut), #, camp_pitch, camp_site
                        building = "hotel"),
-  tourism = list(tourism = "",
+  tourism = list(tourism = "!no", # All except generic statements (in those cases the primary feature is something else)
+                 shop = "travel_agency",
                  office = .c(guide, travel_agent)),
-  financial = list(amenity = .c(atm, bank, bureau_de_change),
+  financial = list(amenity = .c(atm, bank, bureau_de_change, mobile_money, mobile_money_agent, money_transfer),
                    office = .c(accountant, financial, financial_advisor, insurance, tax_advisor)),
   health = list(amenity = .c(baby_hatch, clinic, dentist, doctors, hospital, nursing_home, pharmacy, social_facility, veterinary),
                 healthcare = "",
                 building = "hospital"),
-  industrial = list(landuse = .c(industrial, port, railway), # TODO: Port and railway not transport? But OSM suggests to declare it an industrial area.
+  industrial = list(industrial = "", # TODO: Check effects...
+                    man_made = .c(kiln, works), # kiln might be too small...
                     building = "industrial",
-                    man_made = .c(kiln, works)), # kiln might be too small...
-  mining = list(landuse = "quarry",
-                man_made = .c(adit, mineshaft, petroleum_well, tailings_pond)),
-  commerical = list(landuse = "commercial",   # Predominantly commercial businesses and their offices.
-                    building = "commercial"), # Could also be shops / malls etc. this is not a very specific tag...
-  construction = list(landuse = "construction",
-                      # man_made = "crane", # A stationary, permanent crane. So not really for construction...
-                      building = "construction"), # TODO: save start_date: The (approximated) date when the building was finished!!!
+                    landuse = .c(industrial, port, railway)), # TODO: Port and railway not transport? But OSM suggests to declare it an industrial area.),
+  mining = list(man_made = .c(adit, mineshaft, petroleum_well, tailings_pond),
+                landuse = "quarry"),
+  commerical = list(building = "commercial",  # Could also be shops / malls etc. this is not a very specific tag...
+                    landuse = "commercial"),  # Predominantly commercial businesses and their offices.
+  construction = list(# man_made = "crane", # A stationary, permanent crane. So not really for construction...
+                      building = "construction", # TODO: save start_date: The (approximated) date when the building was finished!!!
+                      landuse = "construction"),
   residential = list(building = .c(house, apartments, bungalow, detached, semidetached_house, terrace, dormitory, residential),
                      landuse = "residential"),
-  farming = list(landuse = .c(farmland, farmyard, orchard, vineyard, aquaculture, salt_pond, greenhouse_horticulture, plant_nursery),
+  farming = list(place = "farm",
+                 man_made = "beehive",
                  building = .c(farm, barn, conservatory, cowshed, farm_auxiliary, greenhouse, slurry_tank, stable, sty),
-                 man_made = "beehive"),
-  emergency = list(emergency = ""),
+                 landuse = .c(farmland, farmyard, orchard, vineyard, aquaculture, salt_pond, greenhouse_horticulture, plant_nursery)),
+  emergency = list(emergency = "!no"),
   entertainment = list(amenity = .c(arts_centre, exhibition_centre, brothel, casino, cinema, events_venue, gambling, love_hotel, nightclub, planetarium, stripclub, swingerclub, theatre),
                        leisure = .c(adult_gaming_centre, amusement_arcade, bandstand, dance)),
   sports = list(leisure = .c(fitness_centre, fitness_station, pitch, sports_centre, stadium, swimming_pool, track),
@@ -102,7 +106,7 @@ osm_point_polygon_class <- list(
                     leisure = .c(beach_resort, marina, miniature_golf, common, park, dog_park, water_park, nature_reserve, picnic_table, fishing, garden, playground, swimming_area),
                     landuse = "recreation_ground",
                     building = "civic"), # Very general tag, match last...
-  historic = list(historic = ""),
+  historic = list(historic = "!no"),
   religion = list(amenity = .c(funeral_hall, crematorium, grave_yard, monastery, place_of_mourning, place_of_worship),
                   building = .c(cathedral, chapel, church, kingdom_hall, monastery, mosque, presbytery, shrine, synagogue, temple, religious),
                   office = "religion",
@@ -112,9 +116,9 @@ osm_point_polygon_class <- list(
   institutional = list(office = .c(government, ngo, association, diplomatic, political_party),
                        building = "government",
                        landuse = "institutional"),
-  military = list(landuse = "military",
-                  military = "",
-                  building = .c(military, barracks, bunker)),
+  military = list(military = "",
+                  building = .c(military, barracks, bunker),
+                  landuse = "military"),
   office_other = list(office = "",
                       building = "office"),
   public_service = list(amenity = .c(courthouse, fire_station, police, post_box, post_depot, post_office, prison, ranger_station, townhall, public_building),
@@ -123,13 +127,15 @@ osm_point_polygon_class <- list(
                                  shelter, shower, toilets, water_point, watering_place, refugee_site, vending_machine),
                     building = "toilets"),
   power = list(power = "",
+               utility = "power",
                building = "transformer_tower",
                "tower:type" = "power"),
   waste = list(amenity = .c(sanitary_dump_station, recycling, waste_basket, waste_disposal, waste_transfer_station),
                water = 	"wastewater",
                man_made = "wastewater_plant",
                landuse = "landfill"),
-  utilities_other = list(man_made = .c(water_tower, water_well, water_works, gasometer, pipeline, pump, pumping_rig, pumping_station, reservoir_covered, street_cabinet),
+  utilities_other = list(man_made = .c(water_tower, water_well, water_works, water_tap, water_tank, gasometer,
+                                       pipeline, pump, pumping_rig, pumping_station, reservoir_covered, street_cabinet),
                          water = "reservoir",
                          office = "water_utility",
                          building = .c(digester, service, water_tower))
@@ -140,20 +146,25 @@ osm_point_polygon_class <- list(
 # Also: save classifying tag along with category...
 
 # Ordering according to tag priorities... and likelihood of duplicate matches
-osm_point_polygon_class = colorder(osm_point_polygon_class,
-                                 military, food, craft,
+osm_point_polygon_class = colorder(osm_point_polygon_class_uo,
+                                 military, craft,
                                  education, education_alt,
-                                 health, sports,
+                                 health,
                                  farming, mining, industrial,
-                                 accommodation, tourism, creativity,
-                                 financial, entertainment, institutional, public_service,
+                                 accommodation, food, tourism, creativity, sports,
+                                 entertainment, institutional, public_service,
                                  storage, communications, transport,
+                                 shopping, # Needs to be after tourism, and before financial, as many shops have mobile money agents...
+                                 financial,
                                  religion, construction,
-                                 shopping, residential, historic,
-                                 waste, power, utilities_other,
-                                 facilities,
+                                 residential, historic, # Todo: historic earlier ? e.g. historic mines etc.. needs to be after tourism though..
+                                 waste,
+                                 office_other,
+                                 commerical,
+                                 power, utilities_other, # Power needs to be matched late because some offices are generated by power = generator etc.
                                  recreation,
-                                 office_other, commerical, emergency)
+                                 facilities,
+                                 emergency)
 
 
 # setrelabel(OSM_classification,
@@ -190,7 +201,8 @@ osm_line_class <- list(
   road = list(highway = .c(motorway, motorway_link, trunk, trunk_link, primary, primary_link, secondary, secondary_link, tertiary, tertiary_link)),
   # residential, living_street, service, construction)), # road, cycleway, unclassififed
   railway = list(railway = ""),
-  waterway = list(waterway = "", natural = "water", water = "", man_made = .c(water_works, reservoir_covered, water_tower)),
+  waterway = list(waterway = c("river", ""),
+                  natural = "water", water = "", man_made = .c(water_works, reservoir_covered, water_tower)),
   power = list(power = ""),
   telecom = list(telecom = ""),
   storage = list(man_made = .c(silo, storage_tank),
@@ -210,7 +222,7 @@ osm_line_class <- list(
 #' @export
 'osm_line_info_tags'
 
-osm_line_info_tags <- list(
+osm_line_info_tags_special <- list(
   aerialway = .c(aerialway, occupancy, capacity, duration, detachable, bubble, heating, bicycle, access),
   aeroway = .c(aerialway, surface, lit, navigationaid, iata, icao, amenity, highway),
   pipeline = .c(type, pipeline, depth, inlet, outlet, construction, substance,
@@ -226,6 +238,6 @@ osm_line_info_tags <- list(
   route = .c(access, duration, opening_hours, reservation, motor_vehicle, foot, maxweight, maxlength, maxwidth, maxheight)
 )
 
-osm_line_info_tags = lapply(osm_line_info_tags,
+osm_line_info_tags = lapply(osm_line_info_tags_special,
                             function(x) unique(c(.c(type, ref, name, description, operator, usage, service, capacity, man_made), x,
                                                      .c(length, width, location, origin, place:origin, destination, place:destination, start_date, end_date))))
